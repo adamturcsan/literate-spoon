34d6f0f2602ddfab1488220b0feacf02da79e7e2
Turcsán Ádám <turcsan.adam@legow.hu>
2017-09-21 14:41:47 +0200

    [Fix]  - .travis.yml if stan == on statement  - Remove strct_types declare from tests

--- test/Component/Join/UsingTest.php
+++ test/Component/Join/UsingTest.php
@@ -10,8 +10,6 @@
  *  file that was distributed with this source code.
  */
 
-declare (strict_types = 1);
-
 namespace LegoW\LiterateSpoon\Test\Component\Join;
 
 use LegoW\LiterateSpoon\Component\Join\Using;
